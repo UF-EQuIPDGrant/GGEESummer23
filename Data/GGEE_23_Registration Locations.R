912bdce70d2ccf464d7c006f7f49cc23fa3008e9
Krista Dulany Chisholm <105238714+kdulany@users.noreply.github.com>
2023-10-16 19:38:08 -0400

    update1016 740PM

---
+++ Data/GGEE_23_Registration Locations.R
@@ -0,0 +1,27 @@
+##Registration locations
+
+
+library(tidyverse)
+library(tidytext)
+library(dplyr)
+library(readr)
+library(tidyr)
+library(writexl)
+library(readxl)
+library(textdata)
+library(ggplot2)
+library(scales)
+library(ggrepel)
+
+GGEE_23_Reg_Locations <- read_excel("Data/GGEE_23_Registration Locations.xlsx")
+
+
+Locations <- select(GGEE_23_Reg_Locations, District, Program)|>
+  count(District, Program)
+
+write_xlsx(Locations,"/Users/kristachisholm/Documents/GitHub/GGEESummer23/Data/GGEE_23_Locations_1.xlsx")
+
+
+GGEE_23_Locations_1 <- read_excel("Data/GGEE_23_Locations_1.xlsx")
+
+
