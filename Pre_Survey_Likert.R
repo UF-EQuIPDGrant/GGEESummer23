c5e6b39f2ab275a89674096a5de593fe72355c52
Krista Dulany Chisholm <105238714+kdulany@users.noreply.github.com>
2023-12-15 16:54:07 -0500

    Pre Survey Data

--- Pre_Survey_Likert.R
+++ Pre_Survey_Likert.R
@@ -21,18 +21,21 @@ webshot::install_phantomjs()
 ####################################################################################################################
 
 
-Pre_Exp <- read_excel("GGEESummer23/Data/GGEE_23_Pre_Survey_Likert.xlsx", sheet = 5)
-Pre_Enj <- read_excel("GGEESummer23/Data/GGEE_23_Pre_Survey_Likert.xlsx", sheet = 6)
+Pre_Exp <- read_excel("Data/GGEE_23_Pre_Survey_Likert.xlsx", sheet = 5)
+Pre_Enj <- read_excel("Data/GGEE_23_Pre_Survey_Likert.xlsx", sheet = 6)
 
 ## STACK PRE - EXPERIENCE
 
-ggplot(Pre_Exp, aes(x=Question, y = n, fill = Level, label = round(((n/204)*100), digits=0),'%'))+
+
+aes(label = paste0(value*100,"%"))
+
+ggplot(Pre_Exp, aes(x=Question, y = n, fill = Level, label = round(p, digits=0),"%"))+
   geom_bar(stat="identity", colour='black', size=.5)+
   geom_text(size = 5, position = position_stack(vjust = 0.5))+
   theme_classic()+
   coord_flip()+
   ggtitle("Prior Experience in Programming")+
-  scale_fill_brewer(guide = guide_legend("Rating", reverse = FALSE),palette = "GnBu")+
+  scale_fill_brewer(guide = guide_legend("% of Rating", reverse = TRUE),palette = "GnBu")+
   theme(axis.text.x = element_text(colour = "black", size = 14),
         axis.title.x=element_text(size=14,face="bold"))+
   theme(axis.text.y = element_text(colour = "black", size = 14),
@@ -41,15 +44,14 @@ ggplot(Pre_Exp, aes(x=Question, y = n, fill = Level, label = round(((n/204)*100)
   scale_x_discrete(labels = label_wrap(25)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 210), n.breaks=10)+
   ylab("Number of Students")+
-  theme(
-        legend.title = element_text(size = 14, face = "bold"),
+  theme(legend.title = element_text(size = 14, face = "bold"),
         legend.text = element_text(size = 12))
 
 ggsave(
   filename = "GGEE_23_Summer_Pre_Exp_Stacked.png",
   plot = last_plot(),
   device = "png",
-  path = "/Users/kristadulany/Documents/GitHub/GGEESummer23/Graphs/Pre_Survey Responses/",
+  path = "/Users/kristachisholm/Documents/GitHub/GGEESummer23/Graphs/Pre_Survey Responses/",
   scale = 2,
   width = 6,
   height = 4,
@@ -62,19 +64,19 @@ ggsave(
 
 ## STACK PRE - ENJOY
 
-ggplot(Pre_Enj, aes(x=Question, y = n, fill = Level, label = round(n, digits=0)))+
+ggplot(Pre_Enj, aes(x=Question, y = n, fill = Level, label = round(p, digits=0),"%"))+
   geom_bar(stat="identity", colour='black', size=.5)+
   geom_text(size = 5, position = position_stack(vjust = 0.5))+
   theme_classic()+
   coord_flip()+
   ggtitle("Prior Enjoyment of Programming")+
-  scale_fill_brewer(guide = guide_legend("Rating", reverse = FALSE),palette = "GnBu")+
+  scale_fill_brewer(guide = guide_legend("% of Rating", reverse = TRUE),palette = "GnBu")+
   theme(axis.text.x = element_text(colour = "black", size = 14),
         axis.title.x=element_text(size=14,face="bold"))+
   theme(axis.text.y = element_text(colour = "black", size = 14),
         axis.title.y=element_text(size=14,face="bold"))+
   theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 20, face = "bold"))+
-  scale_x_discrete(labels = label_wrap(15)) +
+  scale_x_discrete(labels = label_wrap(25)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 210), n.breaks=10)+
   ylab("Number of Students")+
   theme(
@@ -85,7 +87,7 @@ ggsave(
   filename = "GGEE_23_Summer_Pre_Enj_Stacked.png",
   plot = last_plot(),
   device = "png",
-  path = "/Users/kristadulany/Documents/GitHub/GGEESummer23/Graphs/Pre_Survey Responses/",
+  path = "/Users/kristachisholm/Documents/GitHub/GGEESummer23/Graphs/Pre_Survey Responses/",
   scale = 2,
   width = 6,
   height = 4,
