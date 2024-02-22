841afe6e0758a15277f75507853d76be8f940599
olivialancaster10 <111524599+olivialancaster10@users.noreply.github.com>
2024-02-22 09:56:18 -0500

    stacked bar charts

--- Data/End of Day/End of Day_23_Graph.R
+++ Data/End of Day/End of Day_23_Graph.R
@@ -148,4 +148,21 @@ ggplot(EoD_Feeling, aes(x=Stage, y=Mean, shape = Question)) +
   
   
 ###STUDENT IDENTITY###
-EoD_Identity <- read_excel("Documents/GitHub/GGEESummer23/Data/End of Day/End of Day_23_Grap
+EoD_Identity <- read_excel("Documents/GitHub/GGEESummer23/Data/End of Day/End of Day_23_Graph.xlsx", sheet =2)
+#View(EoD_Identity)
+
+
+ggplot(EoD_Identity, aes(x=Stage, y=Mean, shape = Question)) + 
+  geom_point(stat="identity", size=3, aes(fill=Question, color = Question))+ 
+  geom_errorbar(aes(ymin=Mean-SD, ymax=Mean+SD), width=.1)+
+  theme_classic()+
+  theme(legend.position = c(.7, .3))+
+  theme(axis.text.x = element_text(colour = "black"))+
+  theme(axis.text.y = element_text(colour = "black"))+
+  ggtitle("Average End of Day Student Identity")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 6), n.breaks=6)+
+  scale_color_brewer(palette = "Set1")+
+  xlab("Activity")+
+  ylab("Average Rating (1-5)")
+
