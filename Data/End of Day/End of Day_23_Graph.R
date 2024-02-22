a392075be0908caf37f6a774cb0556e54132240d
olivialancaster10 <111524599+olivialancaster10@users.noreply.github.com>
2024-02-22 09:55:59 -0500

    stacked bar charts

--- Data/End of Day/End of Day_23_Graph.R
+++ Data/End of Day/End of Day_23_Graph.R
@@ -26,7 +26,7 @@ library(scales)
 ###CODING EXPERIENCE AND ENJOYMENT###
 ####################################################################################################################
 
-EoD_FELT <- read_excel("Data/End of Day/End of Day_23_Graph.xlsx", sheet = 7)
+EoD_FELT <- read_excel("Documents/GitHub/GGEESummer23/Data/End of Day/End of Day_23_Graph.xlsx", sheet = 8)
 
 
 ## STACK PRE - FELT
@@ -36,12 +36,12 @@ EoD_FELT <- read_excel("Data/End of Day/End of Day_23_Graph.xlsx", sheet = 7)
 
 aes(label = paste0(value*100,"%"))
 
-ggplot(EoD_FELT, aes(x=Stage, y = n, fill = Level, label = round(p, digits=0),"%"))+
-  geom_bar(stat="identity", colour='black', size=.5)+
+ggplot(EoD_FELT, aes(x=Question, y = P, fill = Level, label = round(P, digits=0),"%"))+
+  geom_bar(stat="identity", colour='black', size=.5)+facet_grid(~ Stage)
   geom_text(size = 5, position = position_stack(vjust = 0.5))+
   theme_classic()+
   coord_flip()+
-  ggtitle("Prior Experience in Programming")+
+  ggtitle("Student's End of Day Feelings")+
   scale_fill_brewer(guide = guide_legend("% of Rating", reverse = TRUE),palette = "GnBu")+
   theme(axis.text.x = element_text(colour = "black", size = 14),
         axis.title.x=element_text(size=14,face="bold"))+
@@ -50,7 +50,7 @@ ggplot(EoD_FELT, aes(x=Stage, y = n, fill = Level, label = round(p, digits=0),"%
   theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 20, face = "bold"))+
   scale_x_discrete(labels = label_wrap(25)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 210), n.breaks=10)+
-  ylab("Number of Students")+
+  ylab("Percent of Students")+
   theme(legend.title = element_text(size = 14, face = "bold"),
         legend.text = element_text(size = 12))
 
@@ -73,13 +73,28 @@ ggsave(
 
 
 
-
-
-
-
-
-
-
+##clustered bar chart
+ggplot(EoD_FELT) +
+  geom_bar(aes(x = Stage, y = P, fill = Level),
+           position = "stack",
+           stat = "identity") +
+  facet_wrap(~ Question)+
+  ylab("Percent of Students")+
+  xlab("Question")+
+  coord_flip()+
+  ggtitle("Student's End of Day Feelings")+
+  scale_fill_brewer(guide = guide_legend("% of Rating", reverse = TRUE),palette = "GnBu")+
+  theme(axis.text.x = element_text(colour = "black", size = 14),
+        axis.title.x=element_text(size=14,face="bold"))+
+  theme(axis.text.y = element_text(colour = "black", size = 14),
+        axis.title.y=element_text(size=14,face="bold"))+
+  theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 14, face = "bold"))+
+  scale_x_discrete(labels = label_wrap(25)) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 100), n.breaks=10)+
+  ylab("Percent of Students")+
+  theme(legend.title = element_text(size = 14, face = "bold"),
+        legend.text = element_text(size = 12))
+  
 
 
 
@@ -133,21 +148,4 @@ ggplot(EoD_Feeling, aes(x=Stage, y=Mean, shape = Question)) +
   
   
 ###STUDENT IDENTITY###
-EoD_Identity <- read_excel("Documents/GitHub/GGEESummer23/Data/End of Day/End of Day_23_Graph.xlsx", sheet =2)
-#View(EoD_Identity)
-
-
-ggplot(EoD_Identity, aes(x=Stage, y=Mean, shape = Question)) + 
-  geom_point(stat="identity", size=3, aes(fill=Question, color = Question))+ 
-  geom_errorbar(aes(ymin=Mean-SD, ymax=Mean+SD), width=.1)+
-  theme_classic()+
-  theme(legend.position = c(.7, .3))+
-  theme(axis.text.x = element_text(colour = "black"))+
-  theme(axis.text.y = element_text(colour = "black"))+
-  ggtitle("Average End of Day Student Identity")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 6), n.breaks=6)+
-  scale_color_brewer(palette = "Set1")+
-  xlab("Activity")+
-  ylab("Average Rating (1-5)")
-
+EoD_Identity <- read_excel("Documents/GitHub/GGEESummer23/Data/End of Day/End of Day_23_Grap
