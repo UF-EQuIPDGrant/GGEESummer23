61e91dee72ac9eb3f61b31ea5402d81fb2492973
Krista Dulany <105238714+kdulany@users.noreply.github.com>
2024-03-06 11:18:22 -0500

    Stacked Cluster Graphs

--- Data/End of Day/End of Day_23_Graph.R
+++ Data/End of Day/End of Day_23_Graph.R
@@ -26,7 +26,7 @@ library(ggrepel)
 ###CODING EXPERIENCE AND ENJOYMENT###
 ####################################################################################################################
 
-EoD_FELT <- read_excel("End of Day_23_Graph.xlsx", sheet = 8)
+EoD_FELT <- read_excel("Data/End of Day/End of Day_23_Graph.xlsx", sheet = 8)
 
 ## STACK PRE - FELT
 
@@ -74,20 +74,18 @@ ggsave(
 
 ##clustered bar chart
 
-stages<- c("Programming Basics", "Micro:bit Pet", "Technical Design Challenge")
-
 ggplot(EoD_FELT)+
   geom_bar(aes(x = Stage, y = P, fill = Level),
            position = position_stack(reverse="True"),
-           stat = "identity")+
-  scale_x_discrete(limits=stages)+
-  ##geom_text(aes(label = paste0(round(P, digits=0),"%"),x=Stage, y=P),size = 3, position = position_stack(vjust = 0.95, reverse=TRUE))+
+           stat = "identity",
+           color = "black")+
+  #geom_text(aes(label = paste0(round(P, digits=0),"%"),x=Stage, y=P),size = 3, position = position_stack(vjust = 0.5, reverse=TRUE))+
   geom_text(aes(label = round(P, digits=0), x=Stage, y = P, group = Level), size = 3, position = position_stack(vjust = 0.5, reverse=TRUE))+
+  scale_x_discrete(limits=c('Technical Design Challenge', 'Micro:bit Pet','Programming Basics' ), labels = label_wrap(25))+
   coord_flip()+
   theme_classic()+
-  theme(strip.text = element_text(size = 14))+
+  theme(strip.text = element_text(size = 14), strip.background=element_rect(size=1))+
   facet_wrap(~ Question, ncol=1, scales = 'free_y', labeller= labeller(group= label_wrap_gen(width= 25)))+
-  ylab("Percent of Students")+
   ggtitle("Student's End of Day Feelings")+
   scale_fill_brewer(guide = guide_legend("Rating", reverse = FALSE),palette = "GnBu")+
   theme(axis.text.x = element_text(colour = "black", size = 14),
@@ -95,9 +93,9 @@ ggplot(EoD_FELT)+
   theme(axis.text.y = element_text(colour = "black", size = 12),
         axis.title.y=element_text(size=14,face="bold"))+
   theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 18, face = "bold"))+
-  scale_x_discrete(labels = label_wrap(25)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 100), n.breaks=3)+
   ylab("Percent of Students")+
+  xlab("Camp Activity")+
   theme(legend.title = element_text(size = 14, face = "bold"),
         legend.text = element_text(size = 12))
 
@@ -106,7 +104,7 @@ ggsave(
   filename = "GGEE_23_Summer_EoD_Stacked_Clustered_0304.png",
   plot = last_plot(),
   device = "png",
-  path = "/Users/oliviarandell/Documents/GitHub/GGEESummer23/Graphs/End of Day/",
+  path = "/Users/kristadulany/Documents/GitHub/GGEESummer23/Graphs/End of Day/",
   scale = 2,
   width = 6,
   height = 4,
@@ -116,8 +114,10 @@ ggsave(
   bg = NULL)
 
 
-
-
+#oliviarandell
+#kristadulany
+#kristachisholm
+#strip.background = element_blank()
 
 
 
