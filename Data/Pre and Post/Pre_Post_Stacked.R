8de2642f8008f69c1d14b566fee215c9af13c55f
Krista Dulany <105238714+kdulany@users.noreply.github.com>
2024-03-26 11:49:23 -0400

    0326 updates

--- Data/Pre and Post/Pre_Post_Stacked.R
+++ Data/Pre and Post/Pre_Post_Stacked.R
@@ -19,32 +19,34 @@ library(webshot)
 
 ##Stacked bar chart###
 ######################SKILL LEVEL###########  
-Pre_Post_Skill<-read_excel("Pre_Post_Stacked.xlsx", sheet=4)
+Pre_Post_Skill<-read_excel("Data/Pre and Post/Pre_Post_Stacked.xlsx", sheet=4)
 ggplot(Pre_Post_Skill)+
   geom_bar(aes(x = Stage, y = P, fill = Level),
            position = position_stack(reverse="True"),
            stat = "identity",
            color = "black")+
   #geom_text(aes(label = paste0(round(P, digits=0),"%"),x=Stage, y=P),size = 3, position = position_stack(vjust = 0.5, reverse=TRUE))+
-  geom_text(aes(label = round(P, digits=0), x=Stage, y = P, group = Level), size = 3, position = position_stack(vjust = 0.5, reverse=TRUE))+
+  geom_text(aes(label = round(P, digits=0), x=Stage, y = P, group = Level), size = 5, position = position_stack(vjust = 0.5, reverse=TRUE))+
   coord_flip()+
   theme_classic()+
-  theme(strip.text = element_text(size = 14), strip.background=element_rect(size=1))+
+  theme(strip.text = element_text(size = 20), strip.background=element_rect(size=1))+
   facet_wrap(~ Question, ncol=1, scales = 'free_y', labeller= labeller(group= label_wrap_gen(width= 25)))+
   scale_fill_brewer(guide = guide_legend("Level"),palette = "GnBu")+
-  theme(axis.text.y = element_text(colour = "black", size = 12),
-        axis.title.y=element_text(size=14,face="bold"))+
-  theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 18, face = "bold"))+
+  theme(axis.text.y = element_text(colour = "black", size = 20),
+        axis.title.y=element_text(size=20,face="bold"),
+        axis.text.x = element_text(colour = "black", size = 20),
+        axis.title.x=element_text(size=20,face="bold"))+
+  theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 24, face = "bold"))+
   scale_y_continuous(expand = c(0, 0), limits = c(0, 100), n.breaks=3)+
   ylab("Percent of Students")+
   xlab("")+
-  theme(legend.title = element_text(size = 14, face = "bold"),
-        legend.text = element_text(size = 12))
+  theme(legend.title = element_text(size = 20, face = "bold"),
+        legend.text = element_text(size = 18))
 ggsave(
-  filename = "GGEE_23_Summer_Pre_Post_Stacked_Skill_0321.png",
+  filename = "GGEE_23_Summer_Pre_Post_Stacked_Skill_0326.png",
   plot = last_plot(),
   device = "png",
-  path = "/Users/oliviarandell/Documents/GitHub/GGEESummer23/Graphs/Pre Post Survey/",
+  path = "/Users/kristadulany/Documents/GitHub/GGEESummer23/Graphs/Pre Post Survey/",
   scale = 2,
   width = 6,
   height = 4,
@@ -55,35 +57,35 @@ ggsave(
 
 ####Stacked######
 ####################Enjoyment########################
-Pre_Post_Enjoy<-read_excel("Pre_Post_Stacked.xlsx", sheet=3)
+Pre_Post_Enjoy<-read_excel("Data/Pre and Post/Pre_Post_Stacked.xlsx", sheet=3)
 ggplot(Pre_Post_Enjoy)+
   geom_bar(aes(x = Question, y = P, fill = Level),
            position = position_stack(reverse="True"),
            stat = "identity",
            color = "black")+
   #geom_text(aes(label = paste0(round(P, digits=0),"%"),x=Stage, y=P),size = 3, position = position_stack(vjust = 0.5, reverse=TRUE))+
-  geom_text(aes(label = round(P, digits=0), x=Question, y = P, group = Level), size = 3, position = position_stack(vjust = 0.5, reverse=TRUE))+
+  geom_text(aes(label = round(P, digits=0), x=Question, y = P, group = Level), size = 5, position = position_stack(vjust = 0.5, reverse=TRUE))+
   coord_flip()+
   scale_x_discrete(label=label_wrap(25))+
   theme_classic()+
-  theme(strip.text = element_text(size = 14), strip.background=element_rect(size=1))+
-  facet_wrap(~ Question, ncol=1, scales = 'free_y', labeller= labeller(group= label_wrap_gen(width= 25)))+
+  theme(strip.text = element_text(size = 18), strip.background=element_rect(size=1))+
+  #facet_wrap(~ Question, ncol=1, scales = 'free_y', labeller= labeller(group= label_wrap_gen(width= 25)))+
   scale_fill_brewer(guide = guide_legend("Level"),palette = "GnBu")+
-  theme(axis.text.x = element_text(colour = "black", size = 14),
-        axis.title.x=element_text(size=14,face="bold"))+
-  theme(axis.text.y = element_text(colour = "black", size = 12),
-        axis.title.y=element_text(size=14,face="bold"))+
+  theme(axis.text.x = element_text(colour = "black", size = 20),
+        axis.title.x=element_text(size=20,face="bold"))+
+  theme(axis.text.y = element_text(colour = "black", size = 20),
+        axis.title.y=element_text(size=20,face="bold"))+
   theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 18, face = "bold"))+
   scale_y_continuous(expand = c(0, 0), limits = c(0, 100), n.breaks=3)+
   ylab("Percent of Students")+
   xlab("")+
-  theme(legend.title = element_text(size = 14, face = "bold"),
-        legend.text = element_text(size = 12))
+  theme(legend.title = element_text(size = 20, face = "bold"),
+        legend.text = element_text(size = 18))
 ggsave(
-  filename = "GGEE_23_Summer_Pre_Post_Stacked_Enjoy_0321.png",
+  filename = "GGEE_23_Summer_Pre_Post_Stacked_Experience_0326.png",
   plot = last_plot(),
   device = "png",
-  path = "/Users/oliviarandell/Documents/GitHub/GGEESummer23/Graphs/Pre Post Survey/",
+  path = "/Users/kristadulany/Documents/GitHub/GGEESummer23/Graphs/Pre Post Survey/",
   scale = 2,
   width = 6,
   height = 4,
